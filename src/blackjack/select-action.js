681568a09cbf03f977601cc6d7f38097b751fcaf
Alex T Leach <alextleach@gmail.com>
2016-06-27 20:35:22 -0500

    struggling on select-action

--- src/blackjack/select-action.js
+++ src/blackjack/select-action.js
@@ -1,3 +1,21 @@
+import cardValue from 'blackjack/card-value'
+import cardSum from 'blackjack/card-sum'
+import shouldHit from 'blackjack/should-hit'
+import shouldSplit from 'blackjack/card-value'
 export default function selectAction(cardA, cardB) {
 
+var y = cardValue(cardA);
+var z = cardValue(cardB);
+var x = cardValue(cardA) + cardValue(cardB);
+
+
+if(cardSum(y,z) === 21){
+  return 'stay';
+}
+if(shouldHit(y,z) === true){
+  return 'hit';
+}
+if(shouldSplit(y,z) === true){
+  return 'split';
+}
 }
--- src/blackjack/should-hit.js
+++ src/blackjack/should-hit.js
@@ -4,6 +4,12 @@ export default function shouldHit(cardA, cardB) {
 if (cardValue(cardA) + cardValue(cardB) < 17){
   return true;
 }
+if (cardValue(cardA) === cardValue(cardB)){
+  return false;
+}
+if (cardValue(cardA) + cardValue(cardB) === 21){
+  return false;
+}
 
 return false;
 }
