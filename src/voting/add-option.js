5225b4fa0f246068ad7e8cbad29bcdca6e7b3769
Alex T Leach <alextleach@gmail.com>
2016-06-29 14:36:41 -0500

    one test left

--- src/voting/add-option.js
+++ src/voting/add-option.js
@@ -1,3 +1,17 @@
 export default function addOption(existingPoll, optionName) {
 
-}
+  let newPoll = [];
+
+    // Duplicate the old array
+    //
+    // Loop through all family members in the old array
+    for (let i = 0; i < existingPoll.length; i++) {
+      // Adds an item to
+      newPoll.push(existingPoll[i]);
+    }
+
+    // Add the new member to the end of the newFamily array
+    newPoll.push({ name: optionName, votes: 0 });
+
+    return newPoll;
+  };
