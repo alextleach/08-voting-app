0f37310df7a0909eaca44080066329cd5fc97393
Alex T Leach <alextleach@gmail.com>
2016-06-29 17:18:36 -0500

    done

--- src/voting/increment-vote.js
+++ src/voting/increment-vote.js
@@ -1,8 +1,16 @@
-export default function addOption(existingPoll, optionIndex) {
+import totalVotes from 'voting/total-votes';
+
+export default function incrementVote(existingPoll, optionIndex) {
   const collector = [];
+  let newObject = {};
 
   for (let i = 0; i < existingPoll.length; i++) {
+
+    newObject = {name: `${existingPoll[i].name}`, votes: existingPoll[i].votes};
+    collector.push(newObject);
+
   }
 
+  collector[optionIndex].votes++;
   return collector;
 }
